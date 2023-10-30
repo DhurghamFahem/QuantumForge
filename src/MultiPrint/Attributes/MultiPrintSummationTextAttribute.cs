da26632dc4d2d58df680cd8f6cbd397bfd0caa3a
Dhurgham <drm.fhm@gmail.com>
2023-10-30 16:57:26 +0300

    working on summation text

---
+++ src/MultiPrint/Attributes/MultiPrintSummationTextAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace MultiPrint.Attributes;
+
+[AttributeUsage(AttributeTargets.All)]
+public class MultiPrintSummationTextAttribute : Attribute
+{
+    protected string SummationTextValue { get; set; } = "";
+    public virtual string SummationText => SummationTextValue;
+
+    public MultiPrintSummationTextAttribute() : this(string.Empty)
+    {
+    }
+
+    public MultiPrintSummationTextAttribute(string summationText)
+    {
+        SummationTextValue = summationText;
+    }
+}
