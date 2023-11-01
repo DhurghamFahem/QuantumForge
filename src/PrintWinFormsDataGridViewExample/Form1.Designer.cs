76909b411fa9fded423ebe6b113712ef60b6b221
Dhurgham <drm.fhm@gmail.com>
2023-11-01 13:45:44 +0300

    add WifForms example

---
+++ src/PrintWinFormsDataGridViewExample/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace PrintWinFormsDataGridViewExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tableLayoutPanel1 = new TableLayoutPanel();
+            flowLayoutPanel1 = new FlowLayoutPanel();
+            btnPrint = new Button();
+            btnGeneratePdf = new Button();
+            btnGenerateXbs = new Button();
+            dataGridView1 = new DataGridView();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column4 = new DataGridViewTextBoxColumn();
+            Column5 = new DataGridViewTextBoxColumn();
+            Column6 = new DataGridViewCheckBoxColumn();
+            tableLayoutPanel1.SuspendLayout();
+            flowLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.Controls.Add(flowLayoutPanel1, 0, 1);
+            tableLayoutPanel1.Controls.Add(dataGridView1, 0, 0);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 50F));
+            tableLayoutPanel1.Size = new Size(800, 450);
+            tableLayoutPanel1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.Controls.Add(btnPrint);
+            flowLayoutPanel1.Controls.Add(btnGeneratePdf);
+            flowLayoutPanel1.Controls.Add(btnGenerateXbs);
+            flowLayoutPanel1.Dock = DockStyle.Fill;
+            flowLayoutPanel1.Location = new Point(0, 400);
+            flowLayoutPanel1.Margin = new Padding(0);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new Size(800, 50);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnPrint
+            // 
+            btnPrint.Location = new Point(3, 3);
+            btnPrint.Name = "btnPrint";
+            btnPrint.Size = new Size(173, 44);
+            btnPrint.TabIndex = 1;
+            btnPrint.Text = "Print";
+            btnPrint.UseVisualStyleBackColor = true;
+            btnPrint.Click += btnPrint_Click;
+            // 
+            // btnGeneratePdf
+            // 
+            btnGeneratePdf.Location = new Point(182, 3);
+            btnGeneratePdf.Name = "btnGeneratePdf";
+            btnGeneratePdf.Size = new Size(173, 44);
+            btnGeneratePdf.TabIndex = 2;
+            btnGeneratePdf.Text = "Generate Pdf";
+            btnGeneratePdf.UseVisualStyleBackColor = true;
+            btnGeneratePdf.Click += btnGeneratePdf_Click;
+            // 
+            // btnGenerateXbs
+            // 
+            btnGenerateXbs.Location = new Point(361, 3);
+            btnGenerateXbs.Name = "btnGenerateXbs";
+            btnGenerateXbs.Size = new Size(173, 44);
+            btnGenerateXbs.TabIndex = 3;
+            btnGenerateXbs.Text = "Generate Xbs";
+            btnGenerateXbs.UseVisualStyleBackColor = true;
+            btnGenerateXbs.Click += btnGenerateXbs_Click;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column1, Column2, Column3, Column4, Column5, Column6 });
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(3, 3);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(794, 394);
+            dataGridView1.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            Column1.DataPropertyName = "Id";
+            Column1.HeaderText = "Id";
+            Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            Column2.DataPropertyName = "Name";
+            Column2.HeaderText = "Name";
+            Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            Column3.DataPropertyName = "Phone";
+            Column3.HeaderText = "Phone";
+            Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            Column4.DataPropertyName = "Birthdate";
+            Column4.HeaderText = "Birthdate";
+            Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            Column5.DataPropertyName = "Balance";
+            Column5.HeaderText = "Balance";
+            Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            Column6.DataPropertyName = "Active";
+            Column6.HeaderText = "Active";
+            Column6.Name = "Column6";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tableLayoutPanel1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            tableLayoutPanel1.ResumeLayout(false);
+            flowLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TableLayoutPanel tableLayoutPanel1;
+        private FlowLayoutPanel flowLayoutPanel1;
+        private Button btnPrint;
+        private Button btnGeneratePdf;
+        private Button btnGenerateXbs;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column4;
+        private DataGridViewTextBoxColumn Column5;
+        private DataGridViewCheckBoxColumn Column6;
+    }
+}
---
+++ src/PrintWinFormsDataGridViewExample/Form1.cs
@@ -0,0 +1,31 @@
+using MultiPrint.Services;
+
+namespace PrintWinFormsDataGridViewExample;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+        var accounts = InvoiceDataSource.GetAccounts();
+        dataGridView1.DataSource = accounts;
+    }
+
+    private void btnPrint_Click(object sender, EventArgs e)
+    {
+    }
+
+    private void btnGeneratePdf_Click(object sender, EventArgs e)
+    {
+        MultiPrintService.GeneratePdf<AccountModel>(dataGridView1.DataSource);
+    }
+
+    private void btnGenerateXbs_Click(object sender, EventArgs e)
+    {
+        MultiPrintService.GenerateXps<AccountModel>(dataGridView1.DataSource);
+    }
+}
